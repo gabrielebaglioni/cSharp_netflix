7ecc7b70e70e585fae3a8eaf3d5359ac67bb3cda
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-10-14 16:17:23 +0200

    add api controller

---
+++ Controllers/api/ApiController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cSharp_netflix.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace cSharp_netflix.Controllers.api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MediaController : Controller
+    {
+        // GET: /<controller>/
+        
+        ApplicationDbContext _ctx;
+        public MediaController()
+        {
+            _ctx = new ApplicationDbContext();
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Actor> actors = _ctx.Actors.ToList();
+            return Ok(actors);
+        }
+    }
+}
+
--- NetfilxContext.cs
+++ NetfilxContext.cs
@@ -19,9 +19,6 @@ public class ApplicationDbContext : DbContext
 
     }
     //DATA SEEDING
-  
-    
-
-
+   
 
 }
