a003250f5c5430edf959621aaade1877ad9d8351
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-10-17 17:40:37 +0200

    add index and search bare with api

--- Controllers/api/HomeController.cs
+++ Controllers/api/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using cSharp_netflix.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,19 +15,36 @@ namespace cSharp_netflix.Controllers.api
     public class HomeController : Controller
     {
         // GET: /<controller>/
-        
+
         ApplicationDbContext _ctx;
         public HomeController()
         {
             _ctx = new ApplicationDbContext();
         }
 
-
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string? name)
         {
-            List<Actor> actors = _ctx.Actors.ToList();
-            return Ok(actors);
+            IQueryable<Actor> actors;
+
+            if (name != null)
+            {
+                actors = _ctx.Actors.Where(actor => actor.Name.ToLower().Contains(name.ToLower()));
+            }
+            else
+            {
+                actors = _ctx.Actors;
+            }
+
+            return Ok(actors.ToList<Actor>());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            Actor actor = _ctx.Actors.Where(e => e.Id == id).FirstOrDefault();
+            //List<Actor> actors = _ctx.Actors.ToList();
+            return Ok(actor);
         }
     }
 }
